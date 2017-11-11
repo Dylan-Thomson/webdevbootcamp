34b68ce4cfde8352bfc86a612edc5707d86118b4
Dylan <djthomson88@gmail.com>
2017-11-11 15:22:58 +0000

    Post Requests Part 1

---
+++ MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.post("/addfriend", function(req, res) {
+    res.send("You have reached the posts route"); 
+});
+
+app.get("/friends", function(req, res) {
+    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    res.render("friends", {friends: friends}); 
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server started...");
+});
