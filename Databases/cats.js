f89df98f2829ce9a0bc80162d6a506595b39e4fb
Dylan <djthomson88@gmail.com>
2017-11-27 15:24:26 +0000

    Introduction to Mongoose Pt 2 - Create and Find

--- Databases/cats.js
+++ Databases/cats.js
@@ -12,5 +12,42 @@ var Cat = mongoose.model("Cat", catSchema); // compile schema into a model
 
 
 // adding a new cat to the DB
+// var george = new Cat({
+//     name: "Luna",
+//     age: 1,
+//     temperament: "Chill"
+// });
+
+// george.save(function(err, cat) {
+//     if(err) {
+//         console.log("Something went wrong... " + err);
+//     }
+//     else {
+//         console.log("We just saved a cat to the database");
+//         console.log(cat);
+//     }
+// });
+
+Cat.create({
+    name: "Leo",
+    age: 9,
+    temperament: "Independent"
+}, function(err, cat) {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(cat);
+    }
+});
 
 // retrieve all cats from the DB and console.log each one
+Cat.find({}, function(err, cats) {
+    if(err) {
+        console.log("Error: " + err);
+    }
+    else {
+        console.log("All cats:");
+        console.log(cats);
+    }
+});
